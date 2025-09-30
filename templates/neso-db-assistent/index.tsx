cc85985310683380b3dfc3aafda6e6e7fa931714
Андрей <andrey@khudoley.pro>
2025-09-30 23:19:55 +0300

    dev: добавлен редирект с индекс-страницы на актуальный веб-интерфейс

--- templates/neso-db-assistent/index.tsx
+++ templates/neso-db-assistent/index.tsx
@@ -1,21 +1,7 @@
 import { jsx } from "@app/html-jsx";
+import { webV1Route } from "./web/v1/index";
 
 app.get("/", async (ctx) => {
-  return (
-    <html>
-      <head>
-        <script src="/s/metric/clarity.js"></script>
-        <style>{`
-          body {
-            height: 100%;
-            width: 100%;
-            background-image: url(https://fs.cdn-chatium.io/thumbnail/image_bXkpfHZFGu.2393x2250.png/s/400x400);
-            background-size: 196px;
-            background-color: #f8f8f8;
-          }
-        `}</style>
-      </head>
-      <body></body>
-    </html>
-  );
+  // Редирект на веб-интерфейс
+  return ctx.resp.redirect(webV1Route.url());
 });
