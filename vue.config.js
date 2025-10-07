e9f0579acb594257db4ffb27f98203166372f130
Андрей <andrey@khudoley.pro>
2025-10-07 06:58:47 +0300

    chore: обновлены пути в конфигурационных файлах для NeSo Assistant

--- vue.config.js
+++ vue.config.js
@@ -2,11 +2,11 @@ module.exports = {
   configureWebpack: {
     resolve: {
       alias: {
-        '@': require('path').resolve(__dirname, './templates/neso-db-assistent/web/v1')
+        '@': require('path').resolve(__dirname, './_stage/neso-assistant/web/v1')
       }
     }
   },
   chainWebpack: config => {
-    config.resolve.alias.set('@', require('path').resolve(__dirname, './templates/neso-db-assistent/web/v1'))
+    config.resolve.alias.set('@', require('path').resolve(__dirname, './_stage/neso-assistant/web/v1'))
   }
 }
